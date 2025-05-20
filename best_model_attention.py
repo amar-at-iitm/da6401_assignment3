9f82fdd31bad0f291931b52ae5b9223e6924a107
Amar <ma24m002@smail.iitm.ac.in>
2025-05-20 19:42:07 +0530

    best model configuration of attention training

---
+++ best_model_attention.py
@@ -0,0 +1,10 @@
+# best_model_attention.py
+
+EMBEDDING_DIM = 128
+HIDDEN_DIM = 256
+ENCODER_LAYERS = 2
+DECODER_LAYERS = 3
+RNN_TYPE = "GRU"
+DROPOUT = 0.3
+BEAM_SIZE = 5
+MODEL_PATH = "best_model_attention.pt"
